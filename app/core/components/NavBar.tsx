729cbec0587203179acef3f23a0c4ea1df34b477
Austin <amayes1991@gmail.com>
2022-05-15 23:41:25 -0500

    meal

--- app/core/components/NavBar.tsx
+++ app/core/components/NavBar.tsx
@@ -2,47 +2,47 @@
 import React from "react"
 import { Heading, Text, Flex, Box, Image } from "@chakra-ui/react"
 import { Head, Link, usePaginatedQuery, useRouter, BlitzPage, Routes } from "blitz"
-import { useCurrentUser } from "app/core/hooks/useCurrentUser"
+// import { useCurrentUser } from "app/core/hooks/useCurrentUser"
 
-const UserInfo = () => {
-  const currentUser = useCurrentUser()
-  const [logoutMutation] = useMutation(logout)
+// const UserInfo = () => {
+//   const currentUser = useCurrentUser()
+//   const [logoutMutation] = useMutation(logout)
 
-  if (currentUser) {
-    return (
-      <>
-        <button
-          className="button small"
-          onClick={async () => {
-            await logoutMutation()
-          }}
-        >
-          Logout
-        </button>
-        <div>
-          User id: <code>{currentUser.id}</code>
-          <br />
-          User role: <code>{currentUser.role}</code>
-        </div>
-      </>
-    )
-  } else {
-    return (
-      <>
-        {/* <Link href={Routes.SignupPage()}>
-        <a className="button small">
-          <strong>Sign Up</strong>
-        </a>
-      </Link> */}
-        <Link href={Routes.LoginPage()}>
-          <a className="button small">
-            <strong>Login</strong>
-          </a>
-        </Link>
-      </>
-    )
-  }
-}
+//   if (currentUser) {
+//     return (
+//       <>
+//         <button
+//           className="button small"
+//           onClick={async () => {
+//             await logoutMutation()
+//           }}
+//         >
+//           Logout
+//         </button>
+//         <div>
+//           User id: <code>{currentUser.id}</code>
+//           <br />
+//           User role: <code>{currentUser.role}</code>
+//         </div>
+//       </>
+//     )
+//   } else {
+//     return (
+//       <>
+//         {/* <Link href={Routes.SignupPage()}>
+//         <a className="button small">
+//           <strong>Sign Up</strong>
+//         </a>
+//       </Link> */}
+//         <Link href={Routes.LoginPage()}>
+//           <a className="button small">
+//             <strong>Login</strong>
+//           </a>
+//         </Link>
+//       </>
+//     )
+//   }
+// }
 
 function NavBar() {
   return (
@@ -74,7 +74,7 @@ function NavBar() {
           Meal Search
         </Text>
       </Link>
-      <UserInfo />
+      {/* <UserInfo /> */}
     </Flex>
   )
 }
