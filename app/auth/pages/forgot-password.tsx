c4c0dd980aeb9e3e943ea741657da8e82548f7ee
Austin <amayes1991@gmail.com>
2022-05-16 04:16:18 -0500

    forgot-pass

--- app/auth/pages/forgot-password.tsx
+++ app/auth/pages/forgot-password.tsx
@@ -4,40 +4,45 @@ import { LabeledTextField } from "app/core/components/LabeledTextField"
 import { Form, FORM_ERROR } from "app/core/components/Form"
 import { ForgotPassword } from "app/auth/validations"
 import forgotPassword from "app/auth/mutations/forgotPassword"
+import { Heading, Text, Flex, Box, Image, Button } from "@chakra-ui/react"
 
 const ForgotPasswordPage: BlitzPage = () => {
   const [forgotPasswordMutation, { isSuccess }] = useMutation(forgotPassword)
 
   return (
     <div>
-      <h1>Forgot your password?</h1>
+      <Box justifyContent="center" alignItems="center" m="5">
+        <Text textAlign="center" bg="whatsapp.300" rounded="full" fontSize="3xl">
+          Forgot your password?
+        </Text>
 
-      {isSuccess ? (
-        <div>
-          <h2>Request Submitted</h2>
-          <p>
-            If your email is in our system, you will receive instructions to reset your password
-            shortly.
-          </p>
-        </div>
-      ) : (
-        <Form
-          submitText="Send Reset Password Instructions"
-          schema={ForgotPassword}
-          initialValues={{ email: "" }}
-          onSubmit={async (values) => {
-            try {
-              await forgotPasswordMutation(values)
-            } catch (error: any) {
-              return {
-                [FORM_ERROR]: "Sorry, we had an unexpected error. Please try again.",
+        {isSuccess ? (
+          <div>
+            <h2>Request Submitted</h2>
+            <p>
+              If your email is in our system, you will receive instructions to reset your password
+              shortly.
+            </p>
+          </div>
+        ) : (
+          <Form
+            submitText="Send Reset Password Instructions"
+            schema={ForgotPassword}
+            initialValues={{ email: "" }}
+            onSubmit={async (values) => {
+              try {
+                await forgotPasswordMutation(values)
+              } catch (error: any) {
+                return {
+                  [FORM_ERROR]: "Sorry, we had an unexpected error. Please try again.",
+                }
               }
-            }
-          }}
-        >
-          <LabeledTextField name="email" label="Email" placeholder="Email" />
-        </Form>
-      )}
+            }}
+          >
+            <LabeledTextField name="email" label="Email" placeholder="Email" />
+          </Form>
+        )}
+      </Box>
     </div>
   )
 }
