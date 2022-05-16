22b0f484c58c0a0af1f8f58423392e56aeb33157
Austin <amayes1991@gmail.com>
2022-05-16 05:07:18 -0500

    nav_bar

--- app/core/components/NavBar.tsx
+++ app/core/components/NavBar.tsx
@@ -4,46 +4,6 @@ import { Heading, Text, Flex, Box, Image } from "@chakra-ui/react"
 import { Head, Link, usePaginatedQuery, useRouter, BlitzPage, Routes } from "blitz"
 // import { useCurrentUser } from "app/core/hooks/useCurrentUser"
 
-// const UserInfo = () => {
-//   const currentUser = useCurrentUser()
-//   const [logoutMutation] = useMutation(logout)
-
-//   if (currentUser) {
-//     return (
-//       <>
-//         <button
-//           className="button small"
-//           onClick={async () => {
-//             await logoutMutation()
-//           }}
-//         >
-//           Logout
-//         </button>
-//         <div>
-//           User id: <code>{currentUser.id}</code>
-//           <br />
-//           User role: <code>{currentUser.role}</code>
-//         </div>
-//       </>
-//     )
-//   } else {
-//     return (
-//       <>
-//         {/* <Link href={Routes.SignupPage()}>
-//         <a className="button small">
-//           <strong>Sign Up</strong>
-//         </a>
-//       </Link> */}
-//         <Link href={Routes.LoginPage()}>
-//           <a className="button small">
-//             <strong>Login</strong>
-//           </a>
-//         </Link>
-//       </>
-//     )
-//   }
-// }
-
 function NavBar() {
   return (
     <Flex
